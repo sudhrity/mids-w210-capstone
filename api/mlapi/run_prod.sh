6777bb80bf3dd683bcb37dfcb638daa96013ee86
Sam Temlock <70182203+stemlock@users.noreply.github.com>
2022-08-04 00:46:09 -0400

    More clean up

--- api/mlapi/run_prod.sh
+++
@@ -1,7 +0,0 @@
-curl -iLX 'GET' \
- 'https://sudhrity.mids-w255.com/predict' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{"text": ["I love you."]}'
-echo
-
--- api/run_k6.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-k6 run  --summary-trend-stats="min,med,avg,max,p(90),p(95),p(99)" load.js
